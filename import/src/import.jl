97b88c082e063e70c37053f98581dbe330fdfbf2
jgduenasl <jgduenasl@unal.edu.co>
2022-12-19 15:16:39 -0500

    add arrow, yaml packages

--- import/src/import.jl
+++ import/src/import.jl
@@ -1,9 +1,41 @@
 # set heading for julia scripts
 #
 
-using CSV, DataFrames
+using ArgParse, YAML, CSV, Arrow, DataFrames
 
+root = dirname(@__FILE__)
 path = "github/julia-course/import/input/SB11_20211.txt"
-data = CSV.read(path, delim="¬", DataFrame)
+inputpath = joinpath(root, "import", "input", "SB11_20211.txt")
+outputpath = joinpath(root, "import", "output", "sb11-20211.parquet")
+statspath = joinpath(root, "import", "output", "stats.yml")
 
+parser = ArgParseSettings()
+@add_arg_table parser begin
+    "--input"
+        help = "file path of data to be imported"
+        arg_type = String
+        default = inputpath
+        required = true
+    "--output"
+        help = "file path of imported data"
+        arg_type = String
+        default = path
+        required = true
+    "--stats"
+        help = "basic stats of the imported file"
+        arg_type = String
+        default = path
+end
+args = ArgParse.parse_args(ARGS, parser)
 
+data = CSV.read(args.input, delim="¬", DataFrame)
+# TODO: use clean_names like R's janitor function to standardize colnames
+# see https://github.com/xiaodaigh/DataConvenience.jl or 
+# https://github.com/TheRoniOne/Cleaner.jl
+
+stats = Dict(:n_rows => nrow(data),
+             :n_cols => ncol(data))
+
+Arrow.write(args.output, data)
+YAML.write_file(args.stats, stats)
+#done
